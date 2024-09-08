369200e299a6bad2f2c12684e0e92b9408731b34
trindadedev13 <devsuay@gmail.com>
2024-09-07 21:49:22 -0300

    fix: Theme fixes

--- app/src/main/java/dev/trindadedev/tinyinstaller/ui/theme/InstallerTheme.kt
+++ app/src/main/java/dev/trindadedev/tinyinstaller/ui/theme/InstallerTheme.kt
@@ -1,8 +1,11 @@
 package dev.trindadedev.tinyinstaller.ui.theme
 
 import android.os.Build
+import android.app.Activity
+
 import android.view.WindowManager
 import androidx.annotation.ChecksSdkIntAtLeast
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
@@ -12,6 +15,7 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
@@ -102,7 +106,6 @@ fun InstallerTheme(
         else -> LightColorScheme
     }
     val view = LocalView.current
-    if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as? Activity)?.window ?: return@SideEffect
 
@@ -125,12 +128,11 @@ fun InstallerTheme(
             WindowCompat.getInsetsController(window, view).isAppearanceLightNavigationBars =
                 !darkTheme
         }
-    }
 
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
-        content = content,
+        content = content
     )
 }
 
